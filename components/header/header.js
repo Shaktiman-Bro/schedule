ee17a1fe323fb64047e9ee339777756baea76ed0
gitproduct <drop@outlook.in>
2021-02-11 12:52:04 +0530

    first commit

---
+++ components/header/header.js
@@ -0,0 +1,17 @@
+import React from 'react'
+// import Logo from ''
+
+function header() {
+    return (
+        <header>
+            <div className="logo">
+                <img src="/images/fav-icon-product-pages.svg" />
+            </div>
+            <nav>
+                Navigation
+            </nav>
+        </header>
+    )
+}
+
+export default header
